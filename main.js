e5f475df0e4c6a2ef3e97cd3eb4ed4446e2b5546
FlorinP <florinpalade1@yahoo.co.uk>
2019-01-24 06:43:16 +0000

    main.js assigned variables to dom elements

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+var alphaButton = document.getElementById('alphaButton');
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+var btn4 = document.getElementById('btn4');
+var div1 = document.getElementById('alphaScore');
+var div2 = document.getElementById('alphaTiming');
